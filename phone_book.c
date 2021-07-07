89a2be5213ffedfc343c11b4bf5db9754344b7e1
Elvis Gijo Paul <84615837+elvisgijo@users.noreply.github.com>
2021-07-08 00:53:44 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -62,7 +62,7 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     exit(0);
   } else if (strcmp(argv[1], "search") == 0) {  /* Handle search */
-    printf("NOT IMPLEMENTED!\n"); /* TBD  */
+    printf("%d",*phone); /* TBD  */
   } else if (strcmp(argv[1], "delete") == 0) {  /* Handle delete */
     if (argc != 3) {
       print_usage("Improper arguments for delete", argv[0]);
